2c1a7147f1830104d5519e46043e9b756cab2126
DrowRanger <54094000+AubidaNaalwa@users.noreply.github.com>
2021-01-24 08:17:56 +0200

    adding Scan

--- deliveryapp/src/App.js
+++ deliveryapp/src/App.js
@@ -8,6 +8,7 @@ import Orders from './components/Orders';
 import MapContainer from './components/MapContainer';
 import Contact from './components/Contact';
 import NavBar from './components/NavBar';
+import Scan from './components/Scan';
 
 
 
@@ -20,6 +21,8 @@ function App(props) {
       <Route exact path='/' render={() =>props.ordersStore.islogIn ? <Orders/> : <LogIn/>} /> 
       <Route exact path='/contact' render={() =>props.ordersStore.islogIn ? <Contact/> : <LogIn/>} /> 
       <Route exact path='/map' render={() =>props.ordersStore.islogIn ? <MapContainer /> : <LogIn/>} />
+      <Route exact path='/Scan' render={() =>props.ordersStore.islogIn ? <Scan /> : <LogIn/>} />
+
     </Router>
   )
 }
